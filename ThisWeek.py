aec2caad8a4411e2769398312704a0df6a7946e1
100082395 <100082395@lubbockisd.net>
2019-10-08 15:58:27 -0500

    Printing escape sequences

--- ThisWeek.py
+++ ThisWeek.py
@@ -1,46 +1,45 @@
 # More strings and text
 
-x = "There are %d types of professionals" % 10 # This is plugging the number in % of the sentence.
-teach = "how to teach" # This variable binary is equal to the binary.
-doNot = "who don't" # This variable doNot is equal to don't.
-y = "Those who know %s and those who %s" % (teach, doNot) # This is plugging in the binary and doNot variable into %s.
+x = "There are %d types of professionals" % 10  # This is plugging the number in % of the sentence.
+teach = "how to teach"  # This variable binary is equal to the binary.
+doNot = "who don't"  # This variable doNot is equal to don't.
+y = "Those who know %s and those who %s" % (teach, doNot)  # This is plugging in the binary and doNot variable into %s.
 
+print(x)  # This will print the x variable with 10 for %d.
+print(y)  # This will print the y variable holding the plugged in variables in parenthesis.
 
-print(x) # This will print the x variable with 10 for %d.
-print(y) # This will print the y variable holding the plugged in variables in parenthesis.
+print("I said: %r." % x)  # This print out the variable plugged in the %r of the sentence.
+print("I also said: '%s'." % y)  # This print out variable plugged in the %s of the sentence.
 
-print("I said: %r." % x) # This print out the variable plugged in the %r of the sentence.
-print("I also said: '%s'." % y) # This print out variable plugged in the %s of the sentence.
+fun = True  # Hilarious is equal to True statement.
+funEvaluation = "How is that fun?! %r"  # The variable equals to the string with the %r (plugged in specifier)
 
-fun = True # Hilarious is equal to True statement.
-funEvaluation = "How is that fun?! %r" # The variable equals to the string with the %r (plugged in specifier)
+print(funEvaluation % fun)  # This print out the latter after the prior sentence is formed.
 
-print(funEvaluation % fun) # This print out the latter after the prior sentence is formed.
-
-w = "This is the left side of..." # W equals to the string.
-e = "a string with a right side." # E equals to string.
-print(w+e) # Print out the two variables within the parenthesis and add them to make a sentence.
+w = "This is the left side of..."  # W equals to the string.
+e = "a string with a right side."  # E equals to string.
+print(w + e)  # Print out the two variables within the parenthesis and add them to make a sentence.
 
 # More printing fun
-print("Once upon a time...") # Print out Mary string
-print("There was something brilliant as %s." % 'snow') # Print out metaphoric string
-print("They found nature upon their travel and make a snow fort.") # Print out Mary's destination
-print("." * 10) # Print out dots 10 times to make a line
-end1 = "C" # end1 equals to C
-end2 = "h" # end2 equals to h
-end3 = "e" # end3 equals to e
-end4 = "e" # end4 equals to e
-end5 = "s" # end5 equals to s
-end6 = "e" # end6 equals to e
-end7 = "B" # end7 equals to B
-end8 = "u" # end8 equals to u
-end9 = "r" # end9 equals to r
-end10 = "g" # end10 equals to g
-end11 = "e" # end11 equals to e
-end12 = "r" # end12 equals to r
+print("Once upon a time...")  # Print out Mary string
+print("There was something brilliant as %s." % 'snow')  # Print out metaphoric string
+print("They found nature upon their travel and make a snow fort.")  # Print out Mary's destination
+print("." * 10)  # Print out dots 10 times to make a line
+end1 = "C"  # end1 equals to C
+end2 = "h"  # end2 equals to h
+end3 = "e"  # end3 equals to e
+end4 = "e"  # end4 equals to e
+end5 = "s"  # end5 equals to s
+end6 = "e"  # end6 equals to e
+end7 = "B"  # end7 equals to B
+end8 = "u"  # end8 equals to u
+end9 = "r"  # end9 equals to r
+end10 = "g"  # end10 equals to g
+end11 = "e"  # end11 equals to e
+end12 = "r"  # end12 equals to r
 
-print(end1 + end2 + end3 + end4 + end5 + end6) # Print out end1, end2, end3, end4, end5, and end6 together.
-print(end7 + end8 + end9 + end10 + end11 + end12) # Print out end7, end8, end9, end10, end11, and end12 together.
+print(end1 + end2 + end3 + end4 + end5 + end6)  # Print out end1, end2, end3, end4, end5, and end6 together.
+print(end7 + end8 + end9 + end10 + end11 + end12)  # Print out end7, end8, end9, end10, end11, and end12 together.
 
 # But wait! There's more:
 formatter = "%r %r %r %r"
@@ -93,17 +92,24 @@ print(topCat)
 
 # Escape Seq            What it does?
 # \\                    Prints Backslash
+print("I'm\\sleepy.")
 # \'                    Prints single-quote
+print("\'")
 # \"                    Prints double quote
-# \a                    ASCII bell makes bell alert sounds
-# \b                    ASCII backspace removes previous character
-# \f                    ASCII formfeed
-# \n                    ASCII ASCII linefeed
-# \N{name}              Prints a character from Unicode database
-# \r
-# \t
-# \uxxxx
-# \Uxxxxxxxx
-# \v
-# \ooo
-# \xhh
+print("\"")
+# \a                    Bell alert sounds
+print("\a")
+# \b                    Backspace removes previous character
+print("You" + "\b" + "Go.")
+# \f                    Formfeed
+print("jump\fbackward")
+# \n                    Linefeed
+print("jump\nbackward")
+# \N{name}              Prints characters from database
+# \r                    Moves all characters after the beginning of the line to next line.
+# \t                    Prints TAB
+# \uxxxx                Print 16-bit
+# \Uxxxxxxxx            Prints 32-bit
+# \v                    \x0b, formfeed
+# \ooo                  Prints character based on octal value
+# \xhh                  Prints character based on hex value
